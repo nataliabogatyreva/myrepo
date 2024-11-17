31eb687840764c4d28cd9a1d99d6f9de671b92cd
“Natalia <reservoir.reine@gmail.com>
2024-11-17 10:59:27 +0100

    Line added

--- Git_test.R
+++ Git_test.R
@@ -4,8 +4,12 @@ library(here)
 
 here("/Users/natalabogatyreva/Library/Mobile Documents/com~apple~CloudDocs/Vault/Science work/GitHub_test/")
 
+#https://github.com/settings/tokens
 gitcreds::gitcreds_set()
 
 usethis::create_from_github(
-  "https://github.com/nataliabogatyreva/myrepo.git"
-)
+  "https://github.com/nataliabogatyreva/myrepo.git",
+  destdir = "~/path/to/where/you/want/the/local/repo/"
+  )
+
+
